29be2a20832236a18d0c75332952cea1441b6991
MaryKotliar <glinskayam@ukr.net>
2023-01-06 15:46:31 +0200

    fix persist

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -6,57 +6,9 @@ import { Container } from './Container/Container';
 import { GlobalStyle } from './GlobalStyle';
 
 import { getContacts } from 'redux/selectors';
-// const initialContacts = [
-//   { id: 'id-1', name: 'Rosie Simpson', number: '459-12-56' },
-//   { id: 'id-2', name: 'Hermione Kline', number: '443-89-12' },
-//   { id: 'id-3', name: 'Eden Clements', number: '645-17-79' },
-//   { id: 'id-4', name: 'Annie Copeland', number: '227-91-26' },
-// ];
 
 export function App() {
-  // const [contacts, setContacts] = useState(() => {
-  //   return (
-  //     JSON.parse(window.localStorage.getItem('contacts')) ?? initialContacts
-  //   );
-  // });
-
-  // const [filter, setFilter] = useState('');
   const contacts = useSelector(getContacts);
-  // useEffect(() => {
-  //   window.localStorage.setItem('contacts', JSON.stringify(contacts));
-  // }, [contacts]);
-
-  // const addContact = contact => {
-  //   const isInContacts = contacts.some(
-  //     ({ name }) => name.toLowerCase() === contact.name.toLowerCase()
-  //   );
-
-  //   if (isInContacts) {
-  //     alert(`${contact.name} is already in contacts`);
-  //     return;
-  //   }
-  //   setContacts(prevContacts => [
-  //     ...prevContacts,
-  //     { id: nanoid(), ...contact },
-  //   ]);
-  // };
-  // const deleteContact = id => {
-  //   setContacts(prevContacts =>
-  //     prevContacts.filter(contact => contact.id !== id)
-  //   );
-  // };
-  // const filterByName = () => {
-  //   const normalizedFilter = filter.toLowerCase();
-
-  //   return contacts.filter(contact =>
-  //     contact.name.toLowerCase().includes(normalizedFilter)
-  //   );
-  // };
-  // const changeFilter = event => {
-  //   setFilter(event.currentTarget.value);
-  // };
-
-  // const filteredContacts = filterByName();
 
   return (
     <Container>
@@ -75,8 +27,3 @@ export function App() {
     </Container>
   );
 }
-// () => {
-//   return (
-//     JSON.parse(window.localStorage.getItem('contacts')) ?? initialContacts
-//   );
-// }
--- src/redux/contactsSlice.js
+++ src/redux/contactsSlice.js
@@ -1,19 +1,21 @@
 import { createSlice, nanoid } from '@reduxjs/toolkit';
 import { persistReducer } from 'redux-persist';
 import storage from 'redux-persist/lib/storage';
-const initialContactsState = [
-  { id: 'id-1', name: 'Rosie Simpson', number: '459-12-56' },
-  { id: 'id-2', name: 'Hermione Kline', number: '443-89-12' },
-  { id: 'id-3', name: 'Eden Clements', number: '645-17-79' },
-  { id: 'id-4', name: 'Annie Copeland', number: '227-91-26' },
-];
+const initialContactsState = {
+  items: [
+    { id: 'id-1', name: 'Rosie Simpson', number: '459-12-56' },
+    { id: 'id-2', name: 'Hermione Kline', number: '443-89-12' },
+    { id: 'id-3', name: 'Eden Clements', number: '645-17-79' },
+    { id: 'id-4', name: 'Annie Copeland', number: '227-91-26' },
+  ],
+};
 const contactsSlice = createSlice({
   name: 'contacts',
   initialState: initialContactsState,
   reducers: {
     addContact: {
       reducer(state, action) {
-        state.push(action.payload);
+        state.items.push(action.payload);
       },
       prepare(newContact) {
         return {
@@ -25,15 +27,17 @@ const contactsSlice = createSlice({
       },
     },
     deleteContact(state, action) {
-      const index = state.findIndex(contact => contact.id === action.payload);
-      state.splice(index, 1);
+      const index = state.items.findIndex(
+        contact => contact.id === action.payload
+      );
+      state.items.splice(index, 1);
     },
   },
 });
 const persistConfig = {
-  key: 'contacts',
+  key: 'abra',
   storage,
-  whitelist: ['contacts'],
+  whitelist: ['items'],
 };
 export const { addContact, deleteContact } = contactsSlice.actions;
 export const contactsReducer = persistReducer(
